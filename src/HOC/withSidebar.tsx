8c67175c1423403c254459dd0ca6a8022f967f8d
ozodbek-dev <ozodbeksoftwaredev@gmail.com>
2023-12-06 16:12:05 +0500

    feature:Widgets in dashboard

--- src/HOC/withSidebar.tsx
+++ src/HOC/withSidebar.tsx
@@ -1,17 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
 import { AdminSidebar } from "@/components";
 import React, { PropsWithChildren } from "react";
 
 function Layout({ children }: PropsWithChildren): JSX.Element {
 	return (
-		<div className='adminContainer'>
+		<div className='admin-container'>
 			<AdminSidebar />
-			{/* Main Content */}
-			<main className="dashboard">{children}</main>
+			<main className='dashboard'>{children}</main>
 		</div>
 	);
 }
 
-function withSidebar<P extends Record<string, unknown>>(WrappedComponent: React.ComponentType<P>) {
+export default function withSidebar<P extends Record<string, unknown>>(WrappedComponent: React.ComponentType<P>) {
 	const componentWithSidebar = (props: P): JSX.Element => {
 		return (
 			<Layout>
@@ -22,5 +22,3 @@ function withSidebar<P extends Record<string, unknown>>(WrappedComponent: React.
 
 	return componentWithSidebar;
 }
-
-export default withSidebar;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,76 @@
 import { withSidebar } from "@/HOC";
- function Dashboard() {
+import { BsSearch } from "react-icons/bs";
+import { FaRegBell } from "react-icons/fa";
+import { HiTrendingUp } from "react-icons/hi";
+
+const userImg: string =
+	"https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png";
+const Dashboard = () => {
 	return (
-    <>
-      
-      <div className="bar"> This is bar</div>
-    </>
+		<>
+			<div className='bar'>
+				<BsSearch />
+
+				<input type='text' placeholder='Search for data, users, docs' />
+
+				<FaRegBell />
+				<img src={userImg} alt='User' />
+			</div>
+			<section className='widget-container'>
+				<WidgetItem heading='Revenue' color='rgb(0,155,255)' value={340000} percent={40} amount={true} />
+
+				<WidgetItem heading='Users' color='rgb(0,198,202 )' value={400} percent={-14} amount={true} />
+
+				<WidgetItem heading='Transactions' color='rgb(0,155,255)' value={23000} percent={80} />
+
+				<WidgetItem heading='Revenue' color='rgb(75,0,255)' value={1000} percent={30} amount={true} />
+			</section>
+
+			<section className='graph-container'>
+				<div className="revenue-chart">
+					<h2>Revenue & Transactions</h2>
+				</div>
+				<div className="dashboard-categories">
+					<h2>Inventory</h2>
+				</div>
+			</section>
+		</>
 	);
+};
+
+interface WidgetItemProps {
+	heading: string;
+	value: number;
+	percent: number;
+	color: string;
+	amount?: boolean;
 }
 
+const WidgetItem = ({ heading, amount = false, value, percent, color }: WidgetItemProps): JSX.Element => (
+	<article className='widget'>
+		<div className='widget-info'>
+			<p>{heading}</p>
+			<h4>{amount ? `$${value}` : value}</h4>
+			{percent > 0 ? (
+				<span className='text-green'>
+					<HiTrendingUp /> + {percent}%{" "}
+				</span>
+			) : (
+				<span className='text-red'>
+					<HiTrendingUp /> + {percent}%{" "}
+				</span>
+			)}
+		</div>
+
+		<div
+			className='widget-circle'
+			style={{
+				background: `conic-gradient(${color} ${(Math.abs(percent) / 100) * 360}deg, rgb(255,255,255) 0)`,
+			}}
+		>
+			<span style={{ color }}>{percent}%</span>
+		</div>
+	</article>
+);
 
 export default withSidebar(Dashboard);
